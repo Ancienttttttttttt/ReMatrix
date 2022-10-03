7f0dc2648202327b70683f6cb2f70e99f67a6f32
Ancienttttttttttt <pila33rus_RU@mail.ru>
2022-10-03 18:31:49 +0300

    3211

--- Figuri/main.py
+++ Figuri/main.py
@@ -5,8 +5,8 @@ window.title("Turtle!")#заголовок
 turtle.shape("turtle")#отрисовка черепашки
 
 #рисование квадрата
-n = 4
-for i in range(n):
+m = 4
+for i in range(m):
     turtle.forward(100)#смещение черепашки на 100 пикселей
     turtle.right(90)#черепашка поварачивается на 90 градусов
 
@@ -26,6 +26,18 @@ turtle.pendown()
 for i in range(n):
     turtle.forward(100)
     turtle.left(180 - angle)#вычитаем из 180 угол поворота
+
+#рисование 5-угольника
+i = 5
+angle1 = 60
+turtle.penup()
+turtle.right(180 - angle1)
+turtle.forward(150)
+turtle.pendown()
+x = (i - 2) * 180 / i
+for j in range(i):
+    turtle.forward(100)
+    turtle.left(180 - x)
 
 
 window.exitonclick()#закрытие окна
--- rematrix/main.py
+++ rematrix/main.py
@@ -1,22 +1,58 @@
 from random import randint
+
 print('2-мерный массив. A[n][m]. Количество элементов n * m')
 n = int(input())
-massiv = [[0 for i in range(n)] for j in range(n)] #задали матрицу из 0-вых элементов
+class PrintMatrix():
+    def __init__(self, n, massiv):
+        self.n = n
+        self.massiv = massiv
+        self.massiv = [[0 for i in range(self.n)] for j in range(self.n)]  # задали матрицу из 0-вых элементов
 
-for i in range(n):
-    for j in range(n):
-        massiv[i][j] = randint(1,9)#заполнение матрицы случ. числами
+    def add_matrix(self):
+        for i in range(self.n):
+            for j in range(self.n):
+                self.massiv[i][j] = randint(1,9)#заполнение матрицы случ. числами
 
-for i in range(n):
-    for j in range(n):
-        print(massiv[i][j], end=' ')
-    print()#вывод матрицы
-print()#табуляция
-for i in range(n):
-    for j in range(n):
-        if i == j:
-            massiv[i][j] = 20#на главной диагонали меняем элемент
-for i in range(n):
-    for j in range(n):
-        print(massiv[i][j], end=' ')
-    print()#опять выводим новый массив
+    def printfirstmatrix(self):
+        for i in range(self.n):
+            for j in range(self.n):
+                print(self.massiv[i][j], end=' ')
+            print()#вывод матрицы
+        print()#табуляция
+
+    def zamena(self):
+        for i in range(self.n):
+            for j in range(self.n):
+                if i == j:
+                    self.massiv[i][j] = 20#на главной диагонали меняем элемент
+            print()
+        print()
+
+    def printsecondmassiv(self):
+        for i in range(self.n):
+            for j in range(self.n):
+                print(self.massiv[i][j], end=' ')
+            print()#опять выводим новый массив
+
+    def zamenax2(self):
+        for i in range(self.n):
+            for j in range(self.n):
+                if self.massiv[i][j] % 7 == 0:
+                    self.massiv[i][j] = 100  # замена 7-ок на 100
+            print()
+        print()
+
+    def summa(self):
+        s = 0
+        for i in range(self.n):
+            for j in range(self.n):
+                s += self.massiv[i][j]
+        print(s)  # сложение всех элементов матрицы
+    def get_matrix(self):
+        self.get_matrix()
+        return self
+
+
+
+
+
